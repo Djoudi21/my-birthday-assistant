de3b8be6cf789c4e10377c01ffb28b5a5f1684b8
Djoudi21 <abdelkrim@gdpr.dev>
2024-05-09 22:56:11 +0200

    feat: Update layout and add register screen
    - Remove import of Slot, update Stack import
    - Add register screen in RootLayout
    - Refactor Page component to Index
    - Modify Page content structure to SafeAreaView with router links
    - Add Register component for registration functionality

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,5 @@
 import "../global.css";
-import {Slot, Stack} from "expo-router";
+import {Stack} from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { PaperProvider } from 'react-native-paper';
 import {
@@ -12,16 +12,16 @@ export default function RootLayout() {
     const queryClient = new QueryClient()
     return (
         <QueryClientProvider client={queryClient}>
-            <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
                 <PaperProvider>
                   <Stack
                       screenOptions={{
                         headerShown: false,
                       }}>
                     <Stack.Screen name="login" options={{headerShown: true}} />
+                    <Stack.Screen name="register" options={{headerShown: true}} />
+
                   </Stack>
                 </PaperProvider>
-            </ClerkProvider>
         </QueryClientProvider>
     )
 }
