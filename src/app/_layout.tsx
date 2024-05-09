c5937854b662723413ff42fdcfc8bdbd8fd032f7
Djoudi21 <abdelkrim@gdpr.dev>
2024-05-09 22:12:21 +0200

    feat: Add new dependencies and update versions
    Added new dependencies for @clerk/clerk-expo and updated versions of existing packages. This includes adding @tanstack/react-query, expo-application, react-native-paper, and more.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,27 @@
 import "../global.css";
-import { Slot } from "expo-router";
+import {Slot, Stack} from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { PaperProvider } from 'react-native-paper';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import React from "react";
 
-export default function Layout() {
-  return <Slot />;
+export default function RootLayout() {
+    const queryClient = new QueryClient()
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+                <PaperProvider>
+                  <Stack
+                      screenOptions={{
+                        headerShown: false,
+                      }}>
+                    <Stack.Screen name="login" options={{headerShown: true}} />
+                  </Stack>
+                </PaperProvider>
+            </ClerkProvider>
+        </QueryClientProvider>
+    )
 }
